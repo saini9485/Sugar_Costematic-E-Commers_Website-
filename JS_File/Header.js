fffb7ed1b57df6338a47a81167673ee452aa982f
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-09-05 15:08:30 +0530

    Login and register Page

---
+++ JS_File/Header.js
@@ -0,0 +1,6 @@
+let mainLogin = document.querySelector(".mainLoginPage")
+let LoginButton = document.querySelector(".Login")
+
+LoginButton.addEventListener("click", function () {
+        mainLogin.style.display="block"
+  });
--- JS_File/loginPage.js
+++ JS_File/loginPage.js
@@ -1,24 +1,43 @@
 /*1.  JS Start For INPUT And OTP FOR next And Previous */
-
 function move(e,p,c,n){
-    // console.log(e)
    var length = document.getElementById(c).value.length
-//    console.log(length)
 var maxlength = document.getElementById(c).getAttribute("maxlength")
-// console.log(length, maxlength)
 if(length==maxlength){
     if(n!==''){
         document.getElementById(n).focus();
-    } 
+    }
 }
 if(e.key=="Backspace"){
     if(p!==''){
         document.getElementById(p).focus();
     }
-   
 }
 // console.log(e.key)
 }
 /*1.  JS End For INPUT And OTP FOR next And Previous */
 
 
+/*OTP Random Otp  Genration */
+  let requestOtp1 = document.getElementByIdName("requestptp");
+ requestOtp1.addEventListener("click", function () {
+  let randomOtp = Math.random();
+  //let randomNumber = Math.floor(Math.random()*(10-1)+1)
+  randomOtp = Math.floor(randomOtp * 10000);
+  alert("One Time Password:-"+ randomOtp);
+  console.log(randomOtp); 
+});
+
+    
+// let login =  document.getElementsByName("#requestptp");
+
+/*close Area */
+// let closed = document.querySelector(".close");
+// let mainLogin = document.querySelector(".mainLoginPage")
+
+// closed.addEventListener("click", function () {
+//     if( mainLogin.style.display="block"){
+//         mainLogin.style.display="none"
+//     } 
+//   });
+
+  
